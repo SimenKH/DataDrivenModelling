5e0e6e6023cc986c808961ff18c716babc56f6a4
SimenKH <59603002+SimenKH@users.noreply.github.com>
2021-07-14 15:56:44 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,145 @@
+__author__ = "Jakob Aungiers"
+__copyright__ = "Jakob Aungiers 2018"
+__version__ = "2.0.0"
+__license__ = "MIT"
+
+import os
+import json
+import time
+import math
+import matplotlib.pyplot as plt
+from core.data_processor import DataLoader
+from core.model import Model
+import core.model as MDL
+from tensorflow.keras.models import load_model
+import sys
+def plot_results(predicted_data, true_data):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+    plt.plot(predicted_data, label='Prediction')
+    plt.legend()
+    plt.show()
+
+
+def plot_results_multiple(predicted_data, true_data, prediction_len):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+	# Pad the list of predictions to shift it in the graph to it's correct start
+    for i, data in enumerate(predicted_data):
+        padding = [None for p in range(i * prediction_len)]
+        plt.plot(padding + data, label='Prediction')
+        plt.legend()
+    plt.show()
+
+
+def main():
+    old=sys.stdout
+    sys.stdout=open("engine.txt",'w')
+
+    configs = json.load(open('config.json', 'r'))
+    if not os.path.exists(configs['model']['save_dir']): os.makedirs(configs['model']['save_dir'])
+
+    data = DataLoader(
+        os.path.join('data', configs['data']['filename']),
+        configs['data']['train_test_split'],
+        configs['data']['columns']
+    )
+
+    model = Model()
+    model.build_model(configs)
+    x, y = data.get_train_data(
+        seq_len=configs['data']['sequence_length'],
+        normalise=configs['data']['normalise']
+    )
+    
+    '''
+	# in-memory training
+	model.train(
+		x,
+		y,
+		epochs = configs['training']['epochs'],
+		batch_size = configs['training']['batch_size'],
+		save_dir = configs['model']['save_dir']
+	)
+	'''
+    # out-of memory generative training
+    steps_per_epoch = math.ceil((data.len_train - configs['data']['sequence_length']) / configs['training']['batch_size'])
+    
+    model.train_generator(
+        data_gen=data.generate_train_batch(
+            seq_len=configs['data']['sequence_length'],
+            batch_size=configs['training']['batch_size'],
+            normalise=configs['data']['normalise']
+        ),
+        epochs=configs['training']['epochs'],
+        batch_size=configs['training']['batch_size'],
+        steps_per_epoch=steps_per_epoch,
+        save_dir=configs['model']['save_dir']
+    )
+
+    x_test, y_test = data.get_test_data(
+        seq_len=configs['data']['sequence_length'],
+        normalise=configs['data']['normalise']
+    )
+    
+   
+    
+
+    sys.stdout.close()
+    sys.stdout=old
+    
+    #predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'], configs['data']['sequence_length'])
+    #predictions = model.predict_sequence_full(x_test, configs['data']['sequence_length'])
+    #predictions = model.predict_point_by_point(x_test)
+    #print(predictions)
+
+    #plot_results_multiple(predictions, y_test, configs['data']['sequence_length'])
+    # plot_results(predictions, y_test)
+
+
+#if __name__ == '__main__':
+#main()
+
+def TestModel(model_filepath):
+    configs = json.load(open('config.json', 'r'))
+    if not os.path.exists(configs['model']['save_dir']): os.makedirs(configs['model']['save_dir'])
+    model=Model()
+    model.load_model(model_filepath)
+    data = DataLoader(
+        os.path.join('data', configs['data']['filename']),
+        configs['data']['train_test_split'],
+        configs['data']['columns']
+    )
+    x_test, y_test = data.get_test_data(
+        seq_len=configs['data']['sequence_length'],
+        normalise=configs['data']['normalise']
+    ) 
+    predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'], configs['data']['sequence_length'])
+    predictions = model.predict_sequence_full(x_test, configs['data']['sequence_length'])
+    predictions = model.predict_point_by_point(x_test)
+    print(predictions)
+
+main()
+
+
+#TestModel(r"C:\Users\simen\source\repos\LSTM-Neural-Network-for-Time-Series-Prediction\saved_models\07072021-120542-e6.h5")
+
+configs = json.load(open('config.json', 'r'))
+data = DataLoader(
+        os.path.join('data', configs['data']['filename']),
+        configs['data']['train_test_split'],
+        configs['data']['columns']
+    )
+x, y = data.get_train_data(
+        seq_len=configs['data']['sequence_length'],
+        normalise=configs['data']['normalise']
+    )
+
+save_dir="C:/Users/simen/source/repos/LSTM-Neural-Network-for-Time-Series-Prediction/pruned_models/"
+
+model_1=load_model(r"C:\Users\simen\source\repos\LSTM-Neural-Network-for-Time-Series-Prediction\saved_models\07072021-170114-e4.h5")
+MDL.very_small_model(configs,data,model_1,save_dir)
+#open("results.txt",'w')
+#MDL.very_small_model(configs,data,model,save_dir)
